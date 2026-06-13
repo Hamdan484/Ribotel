3020932d8d8367d9bacf5edaa4bab16265881542
Oware Ebenezer <nanaprestige71@gmail.com>
2026-06-13 11:33:41 -0700

    Add autoreply message

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -34,6 +34,7 @@ export default function ContactSection() {
         emailjs.send(
           import.meta.env.VITE_EMAILJS_SERVICE_ID,
           import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
+          import.meta.env.VITE_EMAILJS_AUTO_REPLY_TEMPLATE_ID,
           {
             name: formData.name,
             email: formData.email,
